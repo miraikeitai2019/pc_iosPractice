1845364c537f1edbe24b979dd545be1e3f43d608
Yuri <40351388+YuriIto@users.noreply.github.com>
2019-06-24 01:47:35 +0900

    Add files via upload

---
+++ pciosPra/pciosPra/view3/view3.swift
@@ -0,0 +1,30 @@
+//
+//  view3.swift
+//  pciosPra
+//
+//  Created by yuri on 2019/06/23.
+//  Copyright © 2019 yuri. All rights reserved.
+//
+
+import UIKit
+
+class view3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
